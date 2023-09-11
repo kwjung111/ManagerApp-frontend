3b5814c59b52cde00754a7bf11873468eb337ac8
astralsoo <57825401+astralsoo@users.noreply.github.com>
2023-09-11 10:31:46 +0900

    코멘트 삭제 기능 추가

--- src/components/PostMemo.vue
+++ src/components/PostMemo.vue
@@ -12,6 +12,22 @@ const memos = computed(() =>{
     return props.memos
 })
 
+//코멘트 삭제
+const removeMemo = (seq) => {
+    if (confirm(`코멘트를 삭제하시겠습니까?`)) {
+        axios.post(`${url}/removeMemo`, {
+            memoSeq: seq
+        }).then((res) => {
+            console.log(res)
+            if (res.data.ok) {
+                alert('삭제되었습니다!')
+            }
+            else {
+                alert('다시 시도해 주세요')
+            }
+        })
+    }
+}
 
 </script>
 
@@ -20,6 +36,9 @@ const memos = computed(() =>{
         <td colspan="2"></td>
         <td colspan="1"> <span class="material-symbols-outlined txt-gray">subdirectory_arrow_right</span> </td>
         <td class="memo-td txt-blue">{{ memo.MEMO_WRTR }}</td>
-        <td colspan="5" class="memo-td txt-left"> <span class="txt-blue"> {{ memo.MEMO_REG_DTM }} </span> :  {{ memo.MEMO_CTNTS }}</td>
+        <td colspan="5" class="memo-td txt-left">
+            <span class="txt-blue"> {{ memo.MEMO_REG_DTM }} </span> :  {{ memo.MEMO_CTNTS }}
+            <button class="btn-delete-memo" title="코멘트 삭제" @click="removeMemo(memo.MEMO_SEQ)">X</button>
+        </td>
     </tr>
 </template>
